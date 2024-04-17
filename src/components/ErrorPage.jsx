1104ed4f13e75df62d5ba2edca3387e1be81e29a
RaisaNuzhat <raisa.csecu@gmail.com>
2024-04-17 14:53:31 +0600

    errorpage updated

--- src/components/ErrorPage.jsx
+++ src/components/ErrorPage.jsx
@@ -1,9 +1,15 @@
-
-
+import errorImage from '../assets/not-found-2384304_1280.jpg'
+import { Helmet } from "react-helmet-async";
 const ErrorPage = () => {
     return (
         <div>
-            <h1 className="flex text-center justify-center font-bold text-3xl">not found</h1>
+             <Helmet>
+                <title>
+                    WanderInn|UpdateProfile
+                </title>
+            </Helmet>
+            <img src={errorImage} className='flex justify-center container items-center w-1/2 h-1/2 mx-auto my-10' alt="" />
+
         </div>
     );
 };
